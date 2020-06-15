89612f6a0cb20973272f67fec2ad782bf747d515
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-15 06:35:32 +0000

    Update resize image to mantain aspect ratio

--- src/resize_images.py
+++ src/resize_images.py
@@ -3,7 +3,7 @@ import glob
 from tqdm import tqdm
 from PIL import Image
 from joblib import Parallel, delayed
-
+import argparse
 # to run simply run the command `python resize_images.py`
 
 def resize_and_save(path, output_path, sz: tuple):
@@ -12,9 +12,46 @@ def resize_and_save(path, output_path, sz: tuple):
     im = im.resize(sz, resample=Image.BILINEAR)
     im.save(os.path.join(output_path, fn))
 
+
+def resize_and_mantain(path, output_path, sz: tuple):
+    fn = os.path.basename(path)  
+    img = Image.open(path)
+    size = min(600, sz[0])  
+    old_size = img.size
+    ratio = float(size)/max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    img = img.resize(new_size, resample=Image.BILINEAR)
+    img.save(os.path.join(output_path, fn))
+
+
 if __name__ == '__main__': 
-    input_folder  = "/home/ubuntu/repos/kaggle/melonama/data/jpeg/test/"
-    output_folder = "/home/ubuntu/repos/kaggle/melonama/data/jpeg/test224/"
-    images = glob.glob(os.path.join(input_folder, '*.jpg'))
-    Parallel(n_jobs=16)(
-        delayed(resize_and_save)(i, output_folder, (224, 224)) for i in tqdm(images))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input_folder", 
+        default=None, 
+        type=str, 
+        required=True, 
+        help="Input folder where images exist."
+    )
+    parser.add_argument(
+        "--output_folder", 
+        default=None, 
+        type=str, 
+        required=True, 
+        help="Output folder for images."
+    )
+    parser.add_argument(
+        "--mantain_aspect_ratio", 
+        default=True, 
+        type=bool, 
+        required=True, 
+        help="Whether to mantain aspect ratio of images."
+    )
+    args = parser.parse_args()
+    images = glob.glob(os.path.join(args.input_folder, '*.jpg'))
+    if not args.mantain_aspect_ratio:
+        Parallel(n_jobs=16)(
+            delayed(resize_and_save)(i, args.output_folder, (224, 224)) for i in tqdm(images))
+    else:
+        Parallel(n_jobs=16)(
+            delayed(resize_and_mantain)(i, args.output_folder, (600, 600)) for i in tqdm(images))
