299e85b97b044bc7f49a377f5f4c4d08c905304c
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-17 09:39:36 +0000

    Add weighted BCE

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class SeResnext50_32x4D(nn.Module):
     def __init__(self, pretrained):
         super(SeResnext50_32x4D, self).__init__()
         self.base_model = pretrainedmodels.__dict__['se_resnext50_32x4d'](pretrained=pretrained)
-        self.out   = nn.Linear(2048, 2)
+        self.out   = nn.Linear(2048, 1)
 
     def forward(self, images, targets, weights=None):
         bs, _, _, _ = images.shape
@@ -15,5 +15,15 @@ class SeResnext50_32x4D(nn.Module):
         out = F.adaptive_avg_pool2d(out, 1)
         out = out.reshape(bs, -1)
         out = self.out(out)
-        loss = nn.CrossEntropyLoss(weight=weights)(out, targets)
+
+        # add weighted BCE loss due to class imbalance
+        if weights is not None:
+            weights_ = weights[targets.data.view(-1).long()].view_as(targets)
+            loss_func = nn.BCEWithLogitsLoss(reduction=None)
+            loss = loss_func(out, targets.view(-1,1).type_as(out))
+            loss_class_weighted = loss * weights_
+            loss = loss_class_weighted.mean()
+        else:
+            loss = nn.BCEWithLogitsLoss()(out, targets.view(-1,1).type_as(out))
+        
         return out, loss
