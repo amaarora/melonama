897092067ff70fbce2812e5b70f229c2e766675a
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-21 18:38:11 +1000

    Add a way to do color constancy using shades of gray during image resizing

--- src/resize_images.py
+++ src/resize_images.py
@@ -5,6 +5,8 @@ from PIL import Image
 from joblib import Parallel, delayed
 import argparse
 from fastai2.vision.all import *
+from color_constancy import color_constancy
+
 # to run simply run the command `python resize_images.py`
 
 
@@ -24,7 +26,7 @@ def resize_and_save(path, output_path, sz: tuple):
     im.save(os.path.join(output_path, fn))
 
 
-def resize_and_mantain(path, output_path, sz: tuple):
+def resize_and_mantain(path, output_path, sz: tuple, args):
     # mantain aspect ratio and shorter edge of resized image is 600px
     # from research paper `https://isic-challenge-stade.s3.amazonaws.com/9e2e7c9c-480c-48dc-a452-c1dd577cc2b2/ISIC2019-paper-0816.pdf?AWSAccessKeyId=AKIA2FPBP3II4S6KTWEU&Signature=nQCPd%2F88z0rftMkXdxYG97Nau4Y%3D&Expires=1592222403`
     fn = os.path.basename(path)  
@@ -34,6 +36,9 @@ def resize_and_mantain(path, output_path, sz: tuple):
     ratio = float(size)/min(old_size)
     new_size = tuple([int(x * ratio) for x in old_size])
     img = img.resize(new_size, resample=Image.BILINEAR)
+    if args.cc: 
+        img = color_constancy(np.array(img))
+        img = Image.fromarray(img)
     img.save(os.path.join(output_path, fn))
 
 
@@ -56,6 +61,7 @@ if __name__ == '__main__':
     parser.add_argument("--mantain_aspect_ratio", action='store_true', default=False, help="Whether to mantain aspect ratio of images.")
     parser.add_argument("--pad_resize", default=True, type=bool, help="Whether to pad and resize images.")
     parser.add_argument("--sz", default=256, type=int, help="Whether to pad and resize images.")
+    parser.add_argument("--cc", default=True, action='store_true', help="Whether to do color constancy to the images.")
 
     args = parser.parse_args()
 
@@ -71,4 +77,4 @@ if __name__ == '__main__':
     else:
         print("Resizing images to mantain aspect ratio in a way that the shorter side is {}px but images are rectangular.".format(args.sz))
         Parallel(n_jobs=32)(
-            delayed(resize_and_mantain)(i, args.output_folder, (args.sz, args.sz)) for i in tqdm(images))
+            delayed(resize_and_mantain)(i, args.output_folder, (args.sz, args.sz), args) for i in tqdm(images))
