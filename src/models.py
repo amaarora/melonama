764f2a48aaffb06f4dba65d7a8ccc26151370dd6
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-27 19:00:28 +1000

    Add weighted focal loss

--- src/models.py
+++ src/models.py
@@ -5,18 +5,34 @@ import torch
 from efficientnet_pytorch import EfficientNet
 
 class FocalLoss(nn.Module):
+    "Non weighted version of Focal Loss"
     def __init__(self, alpha=1, gamma=2):
         super(FocalLoss, self).__init__()
-        self.alpha = alpha
         self.gamma = gamma
         
     def forward(self, inputs, targets):
         BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
         pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+        F_loss =  (1-pt)**self.gamma * BCE_loss
         return F_loss.mean()
 
-loss_fn = FocalLoss(0.75, 2)
+
+class WeightedFocalLoss(nn.Module):
+    "Non weighted version of Focal Loss"
+    def __init__(self, alpha=.25, gamma=2):
+        super(WeightedFocalLoss, self).__init__()
+        self.alpha = torch.tensor([alpha, 1-alpha]).cuda()
+        self.gamma = gamma
+        
+    def forward(self, inputs, targets):
+        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        targets = targets.type(torch.long)
+        at = self.alpha.gather(0, targets.data.view(-1))
+        pt = torch.exp(-BCE_loss)
+        F_loss = at*(1-pt)**self.gamma * BCE_loss
+        return F_loss.mean()
+
+loss_fn = WeightedFocalLoss()
 
 class SeResnext50_32x4D(nn.Module):
     def __init__(self, pretrained):
