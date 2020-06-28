3348d25470eb157cd68a655b69d2366872b002ae
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-28 13:22:53 +1000

    Let validation occur

--- src/utils.py
+++ src/utils.py
@@ -44,7 +44,7 @@ def modify_model(model, args, nftrs=2):
             cnn_features = F.dropout(cnn_features, p=0.2, training=self.training)
         meta_features = self.meta_before(meta)
 
-        if not self.training:
+        if not self.training and args.tta:
             meta_features = meta_features.repeat_interleave(5,0)
 
         features = torch.cat([cnn_features, meta_features], dim=1)
