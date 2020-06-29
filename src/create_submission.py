9399d678e096f2abbb59d04b1cac3e3f462aad99
Aman Arora <Aman.arora@corelogic.com.au>
2020-06-29 11:26:48 +1000

    Update submission

--- src/create_submission.py
+++ src/create_submission.py
@@ -30,6 +30,19 @@ if __name__ == '__main__':
     predictions2 = sum(predictions) / len(predictions)
     predictions2 = torch.sigmoid(torch.tensor(predictions2)).numpy()
 
-    predictions = ((0.6*predictions1) + (0.4*predictions2))
+    # 384x384 (with metadata)
+    np_array_paths = [
+        '/home/ubuntu/repos/kaggle/melonama/data/output/efficient_net_fold_0_384_0.9306738004113618.npy',
+        '/home/ubuntu/repos/kaggle/melonama/data/output/efficient_net_fold_1_384_0.916072911542388.npy',
+        '/home/ubuntu/repos/kaggle/melonama/data/output/efficient_net_fold_2_384_0.9167372701855461.npy',
+        '/home/ubuntu/repos/kaggle/melonama/data/output/efficient_net_fold_3_384_0.8967300002366136.npy',
+        '/home/ubuntu/repos/kaggle/melonama/data/output/efficient_net_fold_4_384_0.9124704260879057.npy',
+    ]
+    predictions = [np.load(path) for path in np_array_paths]
+    predictions3 = sum(predictions) / len(predictions)
+    predictions3 = torch.sigmoid(torch.tensor(predictions3)).numpy()
+
+
+    predictions = ((0.6*predictions1) + (0.15*predictions2) + (0.25*predictions3))
     sub['target'] = predictions
     sub.to_csv("/home/ubuntu/repos/kaggle/melonama/data/output/submission.csv", index=False)
